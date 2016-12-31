61bd96e13ecc606090d4a65b05f2ed44939871a9
ShenYj <shenyanjie123@foxmail.com>
2016-12-31 10:24:14 +0800

    first test

---
+++ 01-Day.py
@@ -0,0 +1,15 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 22 2016, 17:23:13) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+>>> print("abc")
+abc
+>>> print("I like" + " You")
+I like You
+>>> print(12+8)
+20
+>>> print (1+1)
+2
+>>> 
