c253d0096f0ecd07e327b274d43aefc9e0c4559c
ShenYj <shenyanjie123@foxmail.com>
2016-12-31 11:18:54 +0800

    first demo

---
+++ 02-Day.py
@@ -0,0 +1,10 @@
+print('first demo')
+temp = input("不妨猜一下XXX现在心里想的是哪个数字:")
+guess = int(temp)
+if guess == 8:
+    print("好厉害好厉害,你是XXX心里的蛔虫吗?")
+    print("哼,猜中了也没有奖励!")
+else:
+    print("猜错啦,XXX现在心里想的是8!")
+
+print("游戏结束,不玩啦^_^")
